d1e8d931483856ad3d3e1281db6871339ece9008
Amine Chograni <aminechograni@gmail.com>
2021-08-01 16:36:20 +0100

    render

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TaskController
+class TaskController extends AbstractController
 {
-    /**
-     * @Route("/Task/")
-     */
-    public function index(): Response
+    public function index()
     {
+        return $this->render('task/index.html.twig');
+    }
 
+    /**
+     * @Route("/task/create")
+     */
+    public function createTask(): Response
+    {
         return new Response(
-            'hello symfony'
+            'hello from create task'
         );
     }
 }
